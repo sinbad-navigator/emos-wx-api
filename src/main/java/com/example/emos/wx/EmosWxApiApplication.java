717c4a39a7c8ed69a578cacb0a6c852356c646bc
nickyang <nick.yang@bytesforce.com>
2023-12-12 18:44:30 +0800

    抵御即跨站脚本（XSS）攻击

--- src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -3,8 +3,10 @@ package com.example.emos.wx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan //让设置的XssFilter过滤器生效
 public class EmosWxApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmosWxApiApplication.class, args);
