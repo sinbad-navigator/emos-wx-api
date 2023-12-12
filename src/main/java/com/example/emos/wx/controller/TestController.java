869fb854a24c8abd164f3f34acc2d2e59dd9de70
nickyang <nick.yang@bytesforce.com>
2023-12-12 16:47:53 +0800

    创建Web方法测试Swagger

---
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.emos.wx.controller;
+
+import com.example.emos.wx.common.util.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //响应的数据都是json形式
+@RequestMapping("/test")
+@Api("测试web接口") //添加这个注解，swagger才会扫描这个类
+public class TestController {
+
+    @GetMapping("/sayHello")
+    @ApiOperation("最简单的测试方法")
+    public R sayHello() {
+        return R.ok().put("message", "hello world");
+    }
+}
